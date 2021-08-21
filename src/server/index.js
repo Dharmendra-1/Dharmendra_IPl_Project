a2438cfc086cd7a0492ab10ce007ce404fefecf1
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-08-21 11:58:16 +0530

    Added json File path for each module and put into array, create convertIntoJson function and pass argument resultOfProblem and jsonFilePath. Inside convertIntoJson function create seperate json file in async way at simultaneously.

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,35 @@ const bestSuperOverEconomies = require('./ipl/superOverEconomie');
 const matchesFilePath = path.join(__dirname, '../data/matches.csv');
 const deliveriesFilePath = path.join(__dirname, '../data/deliveries.csv');
 
+const teamWonMatchAndTossJsonFilePath = path.join(
+  __dirname,
+  '../public/output/teamWonMatchAndToss.json'
+);
+const playerOfTheMatchJsonFilePath = path.join(
+  __dirname,
+  '../public/output/playerOfTheMatch.json'
+);
+const strikeRateOfBatsmanJsonFilePath = path.join(
+  __dirname,
+  '../public/output/outputstrikeRateOfBatsman.json'
+);
+const highestDissmissPlayerJsonFilePath = path.join(
+  __dirname,
+  '../public/output/highestDissmissPlayer.json'
+);
+const superOverEconomiesJsonFilePath = path.join(
+  __dirname,
+  '../public/output/superOverEconomies.json'
+);
+
+const jsonFilePath = [
+  teamWonMatchAndTossJsonFilePath,
+  playerOfTheMatchJsonFilePath,
+  strikeRateOfBatsmanJsonFilePath,
+  highestDissmissPlayerJsonFilePath,
+  superOverEconomiesJsonFilePath,
+];
+
 csv()
   .fromFile(matchesFilePath)
   .then((matches) => {
@@ -24,9 +53,30 @@ csv()
           matches,
           deliveries
         );
-
         const highestDissmissPlayer = highestDissmiss(deliveries);
-
         const superOverEconomies = bestSuperOverEconomies(deliveries);
+
+        const resultOfProblem = [
+          teamWonMatchAndToss,
+          playerOfTheMatch,
+          strikeRateOfBatsman,
+          highestDissmissPlayer,
+          superOverEconomies,
+        ];
+
+        convertIntoJson(resultOfProblem, jsonFilePath);
       });
   });
+
+const convertIntoJson = (resultOfProblem, jsonFilePath) => {
+  let index = 0;
+  resultOfProblem.forEach((matchResult) => {
+    let jsonDataOfMatch = JSON.stringify(matchResult);
+
+    fs.writeFile(jsonFilePath[index++], jsonDataOfMatch, (error, data) => {
+      if (error) {
+        console.error(error.message);
+      }
+    });
+  });
+};
